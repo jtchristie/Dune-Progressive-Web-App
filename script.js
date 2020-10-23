3d1adf1d4494f62ebc744f68a59d9be468e45bcd
Jonathan Christie <jchristie203@gmail.com>
2020-10-22 21:56:48 -0400

    Install application working

--- script.js
+++ script.js
@@ -7,5 +7,48 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
   
+
     });
   });
+
+  if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').then(function(result){ 
+        console.log('Service Worker Registered'); 
+        console.log("Scope: " + result.scope);
+        }, function(error){
+        console.log('Service Worker Registration Failed'); console.log(error.toString());
+        });
+  } else {
+      console.log('Service Worker Not Supported');
+  }
+
+  var installEvt; window.addEventListener('beforeinstallprompt', function(evt){
+    console.log('Before Install Prompt')
+    //Store the event
+    installEvt = evt;
+    //Prevent chrome 67 or less from automatically showing prompt evt.preventDefault();
+        //show the install ui
+    document.getElementById('addToHomeScreen').style.display = 'block'; });
+
+    function hidePrompt(){
+        document.getElementById('addToHomeScreen').style.display = 'none'; 
+    }
+        
+function installApp(){
+//hide the install ui
+hidePrompt();
+console.log("Install App called");
+//show the install prompt 
+installEvt.prompt();
+ installEvt.userChoice.then(function(result){
+if(result.outcome === 'accepted') console.log('App Installed');
+else
+console.log('App Not Installed');
+});
+installEvt = null;
+
+window.addEventListener('appinstalled', function(evt){ //The user installed the app
+    console.log('App Installed');
+    });
+}
+//clear the saved event - it can't be used again anyway 
---
+++ sw.js
@@ -0,0 +1,50 @@
+var cacheName = 'CSv1';
+
+var cachedFiles = [
+    '/',
+    'index.html',
+    'messiah.html',
+    'assets/duneCover.jpg',
+    'main.css',
+    'manifest.json',
+    'emperor.html',
+    'favicon.ico',
+];
+
+self.addEventListener('install', function (evt) { 
+    console.log('Service Worker Install Event'); //Add the files to the cache
+    evt.waitUntil(
+        caches.open(cacheName).then(function (cache) { 
+            console.log('Caching files');
+            return cache.addAll(cachedFiles);
+         }).then(function(){
+             return self.skipWaiting();
+         }).catch (function (err){
+             console.log('Cache Failed', err);
+         })
+    );
+});
+
+self.addEventListener('activate', function (evt) { 
+    console.log('Service Worker Activated');
+    evt.waitUntil(
+        caches.keys().then(function (keyList) { 
+            return Promise.all(keyList.map(function (key) { 
+                if(key !== cacheName) {
+                console.log('Removing Old Cache', key);
+                return caches.delete(key) 
+            }
+        }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt){
+    console.log('Fetch Event ' + evt.request.url)
+    evt.respondWith(
+      caches.match(evt.request).then(function(response){
+          return response|| fetch(evt.request);
+      }) 
+    );
+});
